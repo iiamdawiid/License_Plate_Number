aa26a30e516c28b32cd52457849fbaba03708313
iiamdawiid <96554798+iiamdawiid@users.noreply.github.com>
2021-12-29 20:33:34 -0600

    Create License_Declarations.h

---
+++ License_Declarations.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+std::string randomNum();
+std::string randomLetters();
+void write_To_File(std::string fName, std::string fDOB, std::string fState, std::string fLicense);
+void display_Info(std::string dName, std::string dDOB, std::string dState, std::string license);
