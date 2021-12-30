d04f2824d1661643a1b78dc7d18a4b1dc03bea18
iiamdawiid <96554798+iiamdawiid@users.noreply.github.com>
2021-12-29 21:29:40 -0600

    Update License_Funcs.cpp

--- License_Funcs.cpp
+++ License_Funcs.cpp
@@ -13,7 +13,7 @@ std::string randomLetters() {
 	srand((unsigned int)time(NULL));
 	
 	char alphabet[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-						          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 	for (int i = 0; i < 3; i++) {
 		randNum = rand() % 26;
